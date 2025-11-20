f2bd03b4eda1dfb3e4cd9b7fdeb5c3d9ba2fe58e
miau <4miau@pm.me>
2025-11-20 15:59:16 +0000

    Added currently unused conditions

---
+++ src/router/Router.ts
@@ -0,0 +1,29 @@
+import express from 'express'
+import Vikala from '../client/vikala'
+import { envs } from '../lib/util/environmentVariables'
+
+export default class Router {
+    app: express.Express
+    client: Vikala
+
+    constructor(client: Vikala) {
+        this.client = client
+        this.app = express()
+
+        this.routes()
+    }
+
+    routes() {
+        this.app.listen(envs.port, () => {
+            console.log(`Server is running on port ${envs.port}`)
+        })
+
+        this.app.get('/', (_, res) => {
+            res.send('Vikky is running!')
+        })
+
+        this.app.get('/twitch', (_, res) => {
+            res.send('Twitch endpoint')
+        })
+    }
+}
---
+++ src/tasks/images/GetBooruImage.ts
@@ -0,0 +1,29 @@
+import { Piece } from '@sapphire/pieces'
+import { ApplyOptions } from '@sapphire/decorators'
+
+import Task from '../../lib/mods/Task'
+import { danbooruApi } from '../../lib/util/constants'
+
+declare type booruOptions = {
+    latest?: boolean
+    safe?: boolean
+}
+
+@ApplyOptions<Piece.Options>({ name: 'getbooruimage' })
+export default class GetBooruImage extends Task {
+    client = this.container.client
+
+    async exec(query: string, options?: booruOptions) {
+        const obj = {
+            method: 'GET',
+            url: `${danbooruApi}/${options.latest ? 'posts.json' : 'posts/random.json'}`,
+            params: {
+                tags: query.replaceAll(' ', '_'),
+                rating: options?.safe ? 's' : undefined,
+                limit: 1
+            }
+        }
+
+        return this.client.api.set(obj).call()
+    }
+}
